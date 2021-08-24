1b46f1d200a6b5be8da8a8a20f4ad5852dcd5173
Stephanie dos Santos Amaral <stephanie.amaral@zup.com.br>
2021-08-24 14:37:35 -0300

    Acompanhamento da proposta

--- src/main/java/br/com/zup/edu/stephanie/propostas/controller/PropostaController.java
+++ src/main/java/br/com/zup/edu/stephanie/propostas/controller/PropostaController.java
@@ -3,6 +3,7 @@ package br.com.zup.edu.stephanie.propostas.controller;
 import br.com.zup.edu.stephanie.propostas.model.Proposta;
 import br.com.zup.edu.stephanie.propostas.repository.PropostaRepository;
 import br.com.zup.edu.stephanie.propostas.request.PropostaRequest;
+import br.com.zup.edu.stephanie.propostas.response.PropostaResponse;
 import br.com.zup.edu.stephanie.propostas.response.SolicitarAnaliseResponse;
 import br.com.zup.edu.stephanie.propostas.service.AnaliseSolicitanteService;
 import br.com.zup.edu.stephanie.propostas.validation.ApiErroException;
@@ -50,5 +51,15 @@ public class PropostaController {
 
         URI uri = uriBuilder.path("/propostas/{id}").buildAndExpand(proposta.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PropostaResponse> statusProposta(@PathVariable Long id) {
+        Optional<Proposta> propostaBanco = propostaRepository.findById(id);
+
+        if (propostaBanco.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(new PropostaResponse(propostaBanco.get()));
     }
 }
